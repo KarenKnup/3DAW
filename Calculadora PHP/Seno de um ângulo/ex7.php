e6fa2ab487d5db3efa30ed01d58ecb9a889b383d
KarenKnup <106675809+KarenKnup@users.noreply.github.com>
2023-04-04 10:26:49 -0300

    Add files via upload

---
+++ Calculadora PHP/Seno de um ângulo/ex7.php
@@ -0,0 +1,25 @@
+<html> 
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <h1>Seno de um ângulo:</h1>
+   <form action="ex7.php" method="get">
+     Seno de (em graus)
+     <input type="number" name="n1">
+     <br><br>
+     <input type="submit" value="Calcular">
+   </form>
+    <br>
+  
+    <?php
+      $n=(180*$_GET["n1"])/(M_PI);
+        if (!($n>0)){
+         $n=0;
+        }
+    ?>
+
+    Resultado = <?php echo $n; ?>
+    
+  </body>
+</html>
