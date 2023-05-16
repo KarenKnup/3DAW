d096f3949e4243d69c3466e9d75e798a33191fab
Karen Knup <106675809+KarenKnup@users.noreply.github.com>
2023-05-16 19:05:56 -0300

    Update form_mult.php
    Design atualizado

--- AV1/form_mult.php
+++ AV1/form_mult.php
@@ -19,7 +19,7 @@
    
     if(!(file_exists("multiplas.txt"))){
       $arqDisc=fopen(filename: "multiplas.txt", mode:"w") or die("Erro ao criar arquivo!");
-      $linha = "pergunta;resposta1;resposta2;resposta3;resposta4;resposta5;resposta\n";
+      $linha = "Pergunta;Resposta 1;Resposta 2;Resposta 3;Resposta 4;Resposta 5;Resposta Correta\n";
       fwrite($arqDisc,$linha);
       fclose($arqDisc);
     } 
@@ -32,8 +32,94 @@
 ?>
 
 <html>
+  <head>
+    <style>
+      body{
+        margin: 0;
+        padding: 0;
+        background: #2d3436;
+      }
+      .middle{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+      }
+      .btn{
+        position: relative;
+        display: block;
+        color: white;
+        font-size: 14px;
+        font-family: "montserrat";
+        text-decoration: none;
+        margin: 30px 0;
+        border: 2px solid lightskyblue;
+        padding: 14px 60px;
+        text-transform: uppercase;
+        overflow: hidden;
+        transition: 1s all ease;
+      }
+      .btn::before{
+        background: lightskyblue;
+        content: "";
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        z-index: -1;
+        transition: all 0.6s ease;
+      }
+      
+      .btn1::before{
+        width: 0%;
+        height: 100%;
+      }
+      
+      .btn1:hover::before{
+        width: 100%;
+      }
+      
+      
+      .btn2::before{
+        width: 100%;
+        height: 0%;
+      }
+      .btn2:hover::before{
+        height: 100%;
+      }
+      
+      .btn3::before{
+        width: 100%;
+        height: 0%;
+        transform: translate(-50%,-50%) rotate(45deg);
+      }
+      .btn3:hover::before{
+        height: 380%;
+      }
+      
+      .btn4::before{
+        width: 100%;
+        height: 0%;
+        transform: translate(-50%,-50%) rotate(-45deg);
+      }
+      .btn4:hover::before{
+        height: 380%;
+      }
+    </style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Syncopate:wght@700&display=swap" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>GAME PROJECT</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Syncopate:wght@700&display=swap" rel="stylesheet">
+  </head>
+  
   <body>
-    <center><h2 style="color:green;">Inserção realizada com sucesso!</h2></center>
-    <p><a href="index.html"> Retornar</a></p>
+    <br>
+    <center><h2 style="color:lightskyblue;">Cadastro de pergunta e respostas realizado com sucesso!</h2></center>
+    <a href="index.html" style="font-family: 'Montserrat', sans-serif;color: white;">RETORNAR</a>
   </body>
 </html>
